ba0d0d50ed845923cf9e8c277de5d4efe0084279
Samuel Bowers <samuelbowers08@gmail.com>
2026-09-03 19:22:52 -0400

    Update fonts.tsx

--- assets/fonts.tsx
+++ assets/fonts.tsx
@@ -1,18 +1,8 @@
-import localFont from 'next/font/local'
+import { Orbitron } from 'next/font/google'
 
-export const eurostile = localFont({
-  src: [
-    {
-      path: '../fonts/Eurostile-BoldExtendedTwo.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/Eurostile-ExtendedTwo.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-eurostile',
+export const orbitron = Orbitron({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-orbitron',
   display: 'fallback',
 })
